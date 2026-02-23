2db58ccdcefd11b3f1e39a6a06a5b853072c4327
IM_MIM <163364978+mimiscoder@users.noreply.github.com.>
2026-02-23 19:03:59 +0430

    Add a variable to store user's choice

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Randomly returns “rock”, “paper” or “scissors”.
 function getComputerChoice() {
     // Get the random number(0-2) and store it in a variable
-    let randomValue = Math.floor(Math.random() * 3);
+    let randomNumber = Math.floor(Math.random() * 3);
 
     // According to the random number(0-2) return either Rock, paper or scissor.
     if(randomValue === 0) {
@@ -15,5 +15,7 @@ function getComputerChoice() {
 
 // Take the user choice and returns it
 function getHumanChoice() {
-    
+    // Get the user choice
+    let userChoice = prompt("Please type rock, paper, or scissors:")
 }
+
