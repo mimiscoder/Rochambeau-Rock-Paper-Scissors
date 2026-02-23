ee8790846e973fb931c49e5d9b08d5f604e2c04e
IM_MIM <163364978+mimiscoder@users.noreply.github.com.>
2026-02-23 19:07:52 +0430

    Add semicolon at the end of some code lines

--- script.js
+++ script.js
@@ -5,18 +5,18 @@ function getComputerChoice() {
 
     // According to the random number(0-2) return either Rock, paper or scissor.
     if(randomValue === 0) {
-        return "rock"
+        return "rock";
     }else if(randomValue === 1) {
-        return "paper"
+        return "paper";
     }else {
-        return "scissor"
+        return "scissor";
     }
 }
 
 // Take the user choice and returns it
 function getHumanChoice() {
     // Get the user choice
-    let userChoice = prompt("Please type rock, paper, or scissors:")
-    return userChoice
+    let userChoice = prompt("Please type rock, paper, or scissors:");
+    return userChoice;
 }
 
