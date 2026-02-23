6dd97c311f0be3d28abc681ffb70518d1c3d21c8
IM_MIM <163364978+mimiscoder@users.noreply.github.com.>
2026-02-23 18:33:37 +0430

    Return rock, paper, or scissors based on a random number

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 function getComputerChoice() {
-    // Get the random value and store it in a variable
+    // Get the random number(0-2) and store it in a variable
     let randomValue = Math.floor(Math.random() * 3);
+
+    // According to the random number(0-2) return either Rock, paper or scissor.
+    if(randomValue === 0) {
+        return "rock"
+    }else if(randomValue === 1) {
+        return "paper"
+    }else {
+        return "scissor"
+    }
 }
+
+
